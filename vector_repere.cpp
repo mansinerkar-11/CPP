8b562d3ac66136f152d678bb962dfaaa7f787f0b
Mansi Nerkar <mansinerkar11@gmail.com>
2023-03-01 22:38:04 +0530

    vectors

--- vector_repere.cpp
+++ vector_repere.cpp
@@ -8,7 +8,7 @@ int main()
     int arr[]={1,2,3,4,5,6}; // standard Carray 
     vector<int> v(arr, arr+5); // Initialize vector with Carray
      cout<<v.back() <<" "<<endl;   //output last element of vector
-     cout<<"Element is poped....!"<<endl;
+     cout<<"Element is poped.........!"<<endl;
      v.pop_back();
      cout<<"\nArray after pop operation..."<<endl;
     while (!v.empty() )            //until vector is empty
