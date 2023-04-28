1d111561440fdaec32cb54f75a9f85782a20be5a
Mansi Nerkar <mansinerkar11@gmail.com>
2023-04-28 22:39:47 +0530

    Add files via upload

---
+++ flight_path_using_graph.cpp
@@ -0,0 +1,37 @@
+/*
+---adjacency matrix---
+There are flight paths between cities. If there is a flight between city A and city B then there is an 
+edge between the cities. The cost of the edge can be the time that flight take to reach city B from A,
+ or the amount of fuel used for the journey. Represent this as a graph. The node can be represented by
+  airport name or name of the city. Use adjacency list representation of the graph or use adjacency
+   matrix representation of the graph.Check whether the graph is connected or not. Justify the storage 
+   representation used.
+*/
+
+#include<iostream>
+using namespace std;
+
+
+void info()
+{
+    int numver;
+    cout<<"Enter number of cities: ";
+    cin>>numver;
+    int graph[numver][numver];
+}
+
+
+
+int main()
+{
+    
+
+    return 0;
+}
+
+
+
+
+
+
+
---
+++ graph_representation_DFS_BFS.cpp
@@ -0,0 +1,22 @@
+/*
+Practical no 07
+represent a given graph using adjacency matrix/list to perform DFS and using adjacency list to perform BFS.
+use the map of area around the collage as a graph identify the prominant landmarks as node and perform DFS and BFS
+
+adjacency matrix
+*/
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char numver;
+    cout<<"Enter number of vertices: ";
+    cin>>numver;
+    int arr[numver][numver];
+    
+    return 0;
+}
